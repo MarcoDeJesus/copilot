45518de4954998a2a77414b5f98c034e70399b28
Marco De Jesus <marcodejesus2010@gmail.com>
2023-08-05 00:45:29 -0600

    Adding custom Web Driver s configuration

--- src/test/java/com/marco/automation/ChromeConfiguration.java
+++ src/test/java/com/marco/automation/ChromeConfiguration.java
@@ -2,13 +2,21 @@ package com.marco.automation;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class ChromeConfiguration implements DriverConfiguration{
 
+    private static final String SRC_MAIN_RESOURCES_WEBDRIVER_CHROMEDRIVER = "src/main/resources/webdriver/chromedriver";
+    private static final String WEBDRIVER_CHROME_DRIVER = "webdriver.chrome.driver";
+
     @Override
     public WebDriver getDriver() {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/webdriver/chromedriver");
-        return new ChromeDriver();
+        System.setProperty(WEBDRIVER_CHROME_DRIVER, SRC_MAIN_RESOURCES_WEBDRIVER_CHROMEDRIVER);
+        
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("incognito");
+        
+        return new ChromeDriver(chromeOptions);
     }
     
 }
--- src/test/java/com/marco/automation/FirefoxConfiguration.java
+++ src/test/java/com/marco/automation/FirefoxConfiguration.java
@@ -2,13 +2,29 @@ package com.marco.automation;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.ProfilesIni;
 
 public class FirefoxConfiguration implements DriverConfiguration{
 
+    private static final String SRC_MAIN_RESOURCES_WEBDRIVER_GECKODRIVER = "src/main/resources/webdriver/geckodriver";
+    private static final String WEBDRIVER_GECKO_DRIVER = "webdriver.gecko.driver";
+
     @Override
     public WebDriver getDriver() {
-        System.setProperty("webdriver.gecko.driver", "src/main/resources/webdriver/geckodriver");
-        return new FirefoxDriver();
+        System.setProperty(WEBDRIVER_GECKO_DRIVER, SRC_MAIN_RESOURCES_WEBDRIVER_GECKODRIVER);
+
+        ProfilesIni profile = new ProfilesIni();
+        
+        FirefoxProfile firefoxProfile = profile.getProfile("default");
+        firefoxProfile.setPreference("browser.active_color", "#EE0000");
+
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.setHeadless(false);
+        firefoxOptions.setProfile(firefoxProfile);
+
+        return new FirefoxDriver(firefoxOptions);
     }
     
 }
