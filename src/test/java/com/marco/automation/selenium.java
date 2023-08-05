8ca6fa5a507acfee19c15d7218f88706fd8fac98
Marco De Jesus <marcodejesus2010@gmail.com>
2023-08-05 01:08:45 -0600

    Refactoring to have different test phases

--- src/test/java/com/marco/automation/selenium.java
+++ src/test/java/com/marco/automation/selenium.java
@@ -3,21 +3,30 @@ package com.marco.automation;
 //import static org.junit.Assert.assertEquals;
 
 import org.junit.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class selenium {
+    static LoginPage loginPage;
+
+    @BeforeSuite
+    public static void beforeSuite(){
+        loginPage = new LoginPage();
+    }
+
+    @AfterSuite
+    public static void afterSuite(){
+        loginPage.close();
+    }
 
     @Test
     public void test() {
-        
-        LoginPage loginPage = new LoginPage();
-        String loginPageTitle = loginPage.getPageTitle();
-
         String expected = "Swag Labs";
-        Assert.assertEquals("The page title is incorrect.", expected, loginPageTitle);
 
-        // Close the browser
-        loginPage.close();
+        String actual = loginPage.getPageTitle();
+
+        Assert.assertEquals("The page title is incorrect.", expected, actual);
     }
 
 }
